826a47bc686687b7704c9ee771bfcf12f959f483
JaketheSnake1778 <82610743+JaketheSnake1778@users.noreply.github.com>
2022-01-22 12:06:04 -0500

    Online code now works

--- Ball-Getter-Local.py
+++ Ball-Getter-Local.py
@@ -1,15 +1,17 @@
 import logging
 import cv2
 import numpy as np
+from time import sleep
 
 
 #Make variables to control stuff that will break if its different
 captureX = 1280
 captureY = 720
+circles = None
 
 
 #iniatilize and set camera to capture from with cv2
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 
 #set resolution to capture with cv2, Microsoft lifecam hd 3000 Fov is 62*
 cap.set(3, captureX)
@@ -19,31 +21,22 @@ cap.set(4, captureY)
 while True:
 	#capture frame
 	_, frame = cap.read()
+	try:
+		if frame == None:
+			print("No cam")
+			continue
 
-	#converts the BGR color space of image to HSV color space and blur it
-	#hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+	except:
+		pass
+	#blur then convert frame to b & w
 	blurred = cv2.GaussianBlur(frame, (3, 3), 0)
-	gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-	# Threshold of blue in HSV space
-	#lower_blue = np.array([60, 35, 25])
-	#upper_blue = np.array([180, 255, 255])
-
-	# preparing the mask to overlay; generates a black/white image(pixels w/i color range are white)
-	#im = cv2.inRange(hsv, lower_blue, upper_blue)
 	output = blurred.copy()
-	#masked = cv2.bitwise_or(output, output, mask = im)
 
-	#find contours in prev created mask
-	#contours = cv2.findContours(im, mode = cv2.RETR_TREE, method = cv2.CHAIN_APPROX_NONE)
-	circles = cv2.HoughCircles(image = gray,
-								method = cv2.HOUGH_GRADIENT, 
-								dp = 1, 
-								minDist = 50, 
-								param1 = 100,
-								param2 = 30,
-								minRadius = 10,
-								maxRadius = 50)
+	#Find circles
+	circles = cv2.HoughCircles(image = gray, method = cv2.HOUGH_GRADIENT, dp = 1.1, minDist = 50, param1 = 100, param2 = 30, minRadius = 10, maxRadius = 50)
+	
 	# ensure at least some circles were found
 	if circles is not None:
 		# convert the (x, y) coordinates and radius of the circles to integers
@@ -53,17 +46,18 @@ while True:
 			color = frame[y, x]
 			try:
 				if color[2] < 100:
-					print("its blue")
 					# draw the circle in the output image, then draw a rectangle
 					# corresponding to the center of the circle
 					cv2.circle(output, (x, y), r, (255, 0, 0), 4)
 					cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 0, 255), -1)
-				if color[0] < 100:
-					print("its red")
+					print("Blue ball at:", x, "  ", y)
+
+				if color[0] < 60:
 					# draw the circle in the output image, then draw a rectangle
 					# corresponding to the center of the circle
 					cv2.circle(output, (x, y), r, (0, 0, 255), 4)
 					cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (255, 0, 0), -1)
+					print("Red ball at:", x, "  ", y)
 			except:
 				pass
 
--- Ball-Getter.py
+++ Ball-Getter.py
@@ -2,7 +2,6 @@ from networktables import NetworkTables
 import logging
 import cv2
 import numpy as np
-from cscore import CameraServer
 import math
 
 #Calibrate camera
@@ -11,10 +10,6 @@ FocalLength = 533.2 * 35.75 / 17.375 #FocalLength = Calibration object width in 
 #Make variables to control stuff that will break if its different
 captureX = 1280
 captureY = 720
-
-#CameraServer init stuff
-cs = CameraServer.getInstance()
-output = cs.putVideo("Camera", captureX, captureY)
 
 #For NetworkTables, not needed, but useful
 logging.basicConfig(level=logging.DEBUG)
@@ -32,25 +27,53 @@ cap.set(4, captureY)
 
 #Insert code logic here
 while True:
-    #capture frame
-    _, frame = cap.read()
+	#capture frame
+	_, frame = cap.read()
 
-    #converts the BGR color space of image to HSV color space and blur it
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+	#converts the BGR color space of image to HSV color space and blur it
+	#hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+	blurred = cv2.GaussianBlur(frame, (3, 3), 0)
+	gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
 
-    # Threshold of blue in HSV space
-    lower_blue = np.array([60, 35, 140])
-    upper_blue = np.array([180, 255, 255])
+	# Threshold of blue in HSV space
+	#lower_blue = np.array([60, 35, 25])
+	#upper_blue = np.array([180, 255, 255])
 
-    # preparing the mask to overlay; generates a black/white image(pixels w/i color range are white)
-    im = cv2.inRange(hsv, lower_blue, upper_blue)
+	# preparing the mask to overlay; generates a black/white image(pixels w/i color range are white)
+	#im = cv2.inRange(hsv, lower_blue, upper_blue)
+	output = blurred.copy()
+	#masked = cv2.bitwise_or(output, output, mask = im)
 
-    #find contours in prev created mask
-    contours = cv2.findContours(im, mode = cv2.RETR_TREE, method = cv2.CHAIN_APPROX_NONE)
+	#find contours in prev created mask
+	#contours = cv2.findContours(im, mode = cv2.RETR_TREE, method = cv2.CHAIN_APPROX_NONE)
+	circles = cv2.HoughCircles(image = gray,
+								method = cv2.HOUGH_GRADIENT, 
+								dp = 1.1, 
+								minDist = 50, 
+								param1 = 100,
+								param2 = 30,
+								minRadius = 10,
+								maxRadius = 50)
+	# ensure at least some circles were found
+	if circles is not None:
+		# convert the (x, y) coordinates and radius of the circles to integers
+		circles = np.round(circles[0, :]).astype("int")
+		# loop over the (x, y) coordinates and radius of the circles
+		for (x, y, r) in circles:
+			color = frame[y, x]
+			try:
+				if color[2] < 100:
+					# draw the circle in the output image, then draw a rectangle
+					# corresponding to the center of the circle
+					cv2.circle(output, (x, y), r, (255, 0, 0), 4)
+					cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 0, 255), -1)
+					print("Blue ball at:", x, "  ", y)
 
-
-    print("running")
-
-
-
-cap.release()
+				if color[0] < 60:
+					# draw the circle in the output image, then draw a rectangle
+					# corresponding to the center of the circle
+					cv2.circle(output, (x, y), r, (0, 0, 255), 4)
+					cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (255, 0, 0), -1)
+					print("Red ball at:", x, "  ", y)
+			except:
+				pass
