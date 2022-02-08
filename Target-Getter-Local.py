a6f1a378c22fd24bbf38d21dc6cb96ae2860c0cf
JaketheSnake1778 <82610743+JaketheSnake1778@users.noreply.github.com>
2022-02-08 17:17:32 -0500

    Revised and improved image pre-processing

--- Target-Getter-Local.py
+++ Target-Getter-Local.py
@@ -7,9 +7,9 @@ from time import sleep
 FocalLength = 533.2 * 35.75 / 17.375 #FocalLength = Calibration object width in pixels * Calibration object distance in inches / Calibration object width in inches
 
 #Make variables to control stuff that will break if its different
-captureX = 1280
-captureY = 720
-kernelmatrix = np.ones((5, 5), np.uint8)
+captureX = 640
+captureY = 360
+kernel = np.ones((5, 5), 'uint8')
 
 #iniatilize and set camera to capture from with cv2
 cap = cv2.VideoCapture(0)
@@ -22,86 +22,37 @@ cap.set(4, captureY)
 while True:
     #capture frame
     _, frame = cap.read()
+    output = frame.copy()
 
     #converts the BGR color space of image to HSV color space and blur it
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     #hsv = cv2.GaussianBlur(hsv, (5, 5), 0)
 
     # Threshold of blue in HSV space
-    lower_blue = np.array([200, 35, 50])
-    upper_blue = np.array([240, 100, 100])
+    lower_blue = np.array([60, 35, 100])
+    upper_blue = np.array([160, 255, 255])
 
     # preparing the mask to overlay; generates a black/white image(pixels w/i color range are white)
     im = cv2.inRange(hsv, lower_blue, upper_blue)
-    im = cv2.dilate(im, kernelmatrix)
+    #im = cv2.bilateralFilter(im, 20, 75, 75)
+    im = cv2.erode(im ,kernel, iterations = 1)
+    im = cv2.dilate(im, kernel, iterations = 1)
+    
+    
 
     #find contours in prev created mask
     contours, hierarchy = cv2.findContours(im, mode = cv2.RETR_TREE, method = cv2.CHAIN_APPROX_NONE)
-
-
-    #finds biggest contour, worship this logic
-    #loops through the contours, saving contours and their area(biggest and 2nd biggest contour)
-    ln = 0
-    bc = 0
-    bcid = None
-    for x in contours:
-        contourNum = contours[ln]#crashes if no contours are found, may cause divide by zero issues later, try except pass everything that references cx, cy, cx2, cy2
-        area = cv2.contourArea(contourNum)
-        if area >= bc:
-            bc = area
-            bcid = contourNum
-        ln = ln + 1
-    #Converts black&white img to RGB
-    imwbb = cv2.cvtColor(im, cv2.COLOR_GRAY2RGB)
-
-    if bcid is None:
-        continue
-
-    #Draw box around biggest contour
-    rect = cv2.minAreaRect(bcid)
-    (x, y), (w, h), angle = rect
-    #print(angle)
-    box = cv2.boxPoints(rect)
-    box = np.int0(box)
-    cv2.drawContours(imwbb, [box], 0, (0, 0, 255), 2)
-
-    x = x - 640
-    y = y - 360
-
-    if w > h:
-        correctW = w
-        correctH = h
-    elif h > w:
-        correctW = h
-        correctH = w
-    
-    #Calculates angle to camera
-    anglePerPixY= 62 / captureY
-    anglePerPix = 62 / captureX
-    Angle = x * anglePerPix
-    AngleY = -y * anglePerPixY
+    for cnt in contours:
+        approx = cv2.approxPolyDP(cnt, 0.009 * cv2.arcLength(cnt, True), True)
+        if(len(approx) == 4): 
+            cv2.drawContours(output, [approx], 0, (0, 0, 255), 5)
     
 
-    #Find distance from camera to contour, the int is the width of the object your measuring distance too
-    Distance = 17.375 * FocalLength / correctW
-    Distance = int(Distance)
+    cv2.imshow('Output', output)
+    cv2.imshow('Mask', im)
 
 
-    #Uses pythagorean thereum to find the horizontal distanc to target
-    C = Distance * Distance
-    B = 83 * 83
-    D = C - B
-    HorizontalDistance = 0
-    try:
-        HorizontalDistance = math.sqrt(D)
-    except:
-        pass
-        #print("To close to target")
 
-    #print(x, " ", y, " ", w, " ", h)
-    print(int(bc), " ", Angle, " ", AngleY)
-    #shows image
-    cv2.imshow('Contours', imwbb)
 
     #Needed to function, do not remove, closes windows when q is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
